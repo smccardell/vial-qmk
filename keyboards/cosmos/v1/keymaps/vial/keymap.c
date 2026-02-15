094fbddf1564c7cc1abddc27ace6142a58d7ae53
shawn <smccardell@gmail.com>
2026-02-15 11:50:36 -0600

    updating cosmos layout

--- keyboards/cosmos/v1/keymaps/vial/keymap.c
+++ keyboards/cosmos/v1/keymaps/vial/keymap.c
@@ -19,7 +19,7 @@ enum custom_keycodes {
     ALT_DEL,
     NAV_SPC,
     SYN_ENT,
-    NAV_DEL,
+    NAV_ENT,
     SYN_BSPC,
     OS_NAV,
     OS_SYNUM,
@@ -30,8 +30,12 @@ enum custom_keycodes {
     C_COPY,
     C_PASTE,
     OS_LSFT,
+    OS_RSFT,
     OS_LCTL,
-    RS_ENT
+    OS_RCTL,
+    OS_LALT,
+    OS_RALT,
+    GUI_V
 };
 
 #define C_LEAD QK_LEADER
@@ -40,7 +44,7 @@ enum custom_keycodes {
 #define ALT_DEL ALT_T(KC_DELETE)
 #define NAV_SPC LT(_NAV, KC_SPC)
 #define SYN_ENT LT(_SYNUM, KC_ENTER)
-#define NAV_DEL LT(_NAV, KC_DELETE)
+#define NAV_ENT LT(_NAV, KC_ENTER)
 #define SYN_BSPC LT(_SYNUM, KC_BSPC)
 #define OS_NAV OSL(_NAV)
 #define OS_SYNUM OSL(_SYNUM)
@@ -54,37 +58,39 @@ enum custom_keycodes {
 #define OS_RSFT OSM(MOD_RSFT)
 #define OS_LCTL OSM(MOD_LCTL)
 #define OS_RCTL OSM(MOD_RCTL)
-#define RS_ENT RSFT_T(KC_ENT)
+#define OS_LALT OSM(MOD_LALT)
+#define OS_RALT OSM(MOD_RALT)
+#define GUI_V LGUI(KC_V)
 
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT(
-      KC_ESC,    KC_1,   KC_2,    KC_3,      KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,     KC_9,    KC_0,      KC_LGUI,
+      KC_ESC,     KC_1,    KC_2,    KC_3,      KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,     KC_9,    KC_0,      KC_LGUI,
       KC_TAB,     KC_Q,    KC_W,    KC_F,      KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,     KC_Y,    KC_QUOT,   QK_REP,
-      OS_LSFT,    KC_A,    KC_R,    KC_S,      KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,     KC_I,    KC_O,      OS_RCTL,
-      OS_LCTL,    KC_Z,    KC_X,    KC_C,      KC_D,    KC_V,                         KC_K,    KC_H,    KC_COMM,  KC_DOT,  KC_SLSH,   RS_ENT,
-                           KC_UNDS, C_PASTE,                                                           C_UNDO,  KC_UNDS, 
-                                               ALT_DEL, NAV_SPC,  SYN_ENT,  NAV_DEL,  SYN_BSPC,  OS_FUN,
-                                                        OS_LCTL, C_LEAD,   C_UNDO,   OS_RSFT
+      OS_LSFT,    KC_A,    KC_R,    KC_S,      KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,     KC_I,    KC_O,      OS_RSFT,
+      OS_LCTL,    KC_Z,    KC_X,    KC_C,      KC_D,    KC_V,                         KC_K,    KC_H,    KC_COMM,  KC_DOT,  KC_SLSH,   OS_RCTL,
+                           KC_UNDS, OS_LALT,                                                            OS_RALT,  KC_UNDS, 
+                                               ALT_DEL, NAV_SPC,  SYN_ENT,  NAV_ENT,  SYN_BSPC,  OS_FUN,
+                                                        OS_FUN,   GUI_V,    C_LEAD,   OS_RSFT
   ),
 
     [_QWERTY] = LAYOUT(
-      KC_ESC,  KC_1,  KC_2,       KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,     KC_9,    KC_0,      KC_LGUI,
+      KC_ESC,    KC_1,  KC_2,       KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,     KC_9,    KC_0,      KC_LGUI,
       KC_TAB,    KC_Q,  KC_W,       KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,      QK_REP,
-      OS_LCTL,   KC_A,  KC_S,       KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,     KC_L,    KC_QUOT,   OS_LCTL,
-      OS_LSFT,   KC_Z,  KC_X,       KC_C,    KC_V,    KC_B,                          KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,   RS_ENT,
-                           KC_UNDS, C_PASTE,                                                           C_UNDO,  KC_UNDS, 
-                                               ALT_DEL, NAV_SPC,  SYN_ENT,  NAV_DEL,  SYN_BSPC,  OS_FUN,
-                                                        OS_LCTL, C_LEAD,   C_UNDO,   OS_RSFT
+      OS_LCTL,   KC_A,  KC_S,       KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,     KC_L,    KC_QUOT,   OS_RSFT,
+      OS_LSFT,   KC_Z,  KC_X,       KC_C,    KC_V,    KC_B,                          KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,   OS_RCTL,
+                           KC_UNDS, OS_LALT,                                                           OS_RALT,  KC_UNDS, 
+                                             ALT_DEL, NAV_SPC,  SYN_ENT,  NAV_ENT,  SYN_BSPC,  OS_FUN,
+                                                      OS_FUN,   GUI_V,    C_LEAD,   OS_RSFT
   ),
  
     [_NAV] = LAYOUT(
       _______, _______,   _______,  _______,  _______,   _______,                     _______,         _______,  _______,    _______,  _______,      _______,
       _______, XXXXXXX,   KC_HOME,  KC_UP,    KC_END,    KC_PGUP,                     KC_KP_ASTERISK,  KC_7,     KC_8,       KC_9,     KC_KP_PLUS,   _______,
       _______, KC_DELETE, KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_PGDN,                     KC_KP_SLASH,     KC_4,     KC_5,       KC_6,     KC_KP_MINUS,  _______,
-      _______, C_UNDO,    C_CUT,    C_COPY,   C_PASTE,   C_REDO,                      KC_COMMA,        KC_1,     KC_2,       KC_3,     KC_EQUAL,     _______,
-                          _______,  _______,                                                           KC_0,     KC_KP_DOT, 
+      _______, C_UNDO,    C_CUT,    C_COPY,   C_PASTE,   C_REDO,                      KC_COMM,         KC_1,     KC_2,       KC_3,     KC_EQUAL,     _______,
+                          _______,  _______,                                                                     KC_0,       KC_KP_DOT, 
                                               _______,   _______,  _______,  _______, _______,         _______,
                                                          _______,  _______,  _______, _______
   ),
@@ -93,8 +99,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, _______,  _______,   _______,  _______,   _______,                     _______,         _______,     _______,     _______,     _______,      _______,
       _______, KC_LBRC,  KC_RBRC,   KC_LPRN,  KC_RPRN,   KC_GRAVE,                    KC_KP_ASTERISK,  LSFT(KC_7),  LSFT(KC_8),  XXXXXXX,     KC_KP_PLUS,   _______,
       _______, KC_SCLN,  KC_QUOTE,  KC_PIPE,  KC_UNDS,   KC_BSLS,                     KC_KP_SLASH,     LSFT(KC_4),  LSFT(KC_5),  LSFT(KC_6),  KC_KP_MINUS,  _______,
-      _______, KC_LABK,  KC_RABK,   KC_COLN,  KC_EQUAL,  XXXXXXX,                     KC_KP_DOT,       LSFT(KC_1),  LSFT(KC_2),  LSFT(KC_3),  XXXXXXX,      KC_KP_EQUAL,
-                         _______,   _______,                                                           KC_KP_0,     KC_KP_DOT, 
+      _______, KC_LABK,  KC_RABK,   KC_COLN,  KC_EQUAL,  XXXXXXX,                     KC_COMM,         LSFT(KC_1),  LSFT(KC_2),  LSFT(KC_3),  KC_KP_EQUAL,  _______,
+                         _______,   _______,                                                                        KC_KP_0,     KC_KP_DOT, 
                                               _______,   _______,  _______,  _______, _______,         _______,
                                               _______,   _______,                     _______,         _______
   ),
